5e36410b224046ffd2ef84e55289dd09804fcfbb
RuRuTiaSaMa <1009087450@qq.com>
2019-01-13 21:12:28 +0800

    msm8998-common: Drop libbt-vendor
    * With QTI bluetooth it's no longer needed.
    
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- msm8998.mk
+++ msm8998.mk
@@ -132,11 +132,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
-# Bluetooth
-PRODUCT_PACKAGES += \
-    libbthost_if \
-    libbt-vendor
-
 # Camera
 PRODUCT_PACKAGES += \
     Snap
