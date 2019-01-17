c3eac764796b42d5869046fa200bd13076d78f02
Henrique Pereira <pereirah2001@gmail.com>
2019-01-17 14:41:58 -0200

    msm8998-common: Include ARCore
    Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- msm8998.mk
+++ msm8998.mk
@@ -132,6 +132,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# ARcore
+TARGET_INCLUDE_STOCK_ARCORE := true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
