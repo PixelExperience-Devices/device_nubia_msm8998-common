89c5ec6c412bdbf7b229b19977d479af7a2c4922
RuRuTiaSaMa <1009087450@qq.com>
2019-01-31 15:14:10 +0800

    something forget to add .
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- msm8998.mk
+++ msm8998.mk
@@ -343,6 +343,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.msm.usb.configfs.rc \
     init.qcom.rc \
+    init.pe.rc\
     init.qcom.factory.rc \
     init.qcom.power.rc \
     init.qcom.usb.rc \
