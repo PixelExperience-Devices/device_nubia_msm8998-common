0939e9c17456c6afd4dbd1ada927d7f8a8c10a60
RuRuTiaSaMa <1009087450@qq.com>
2019-02-04 15:06:14 +0800

    add KcalMod for display calibration * modify from zuk_msm8996 Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- msm8998.mk
+++ msm8998.mk
@@ -246,9 +246,9 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
-# LiveDisplay native
+# Display Mod
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    KcalMod
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
