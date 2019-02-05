2b653c063d6cd71ae172749d0bc72b785b547957
RuRuTiaSaMa <1009087450@qq.com>
2019-02-06 00:37:31 +0800

    do not set BOARD_SEPOLICY_VERS
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -256,8 +256,6 @@ BOARD_ROOT_EXTRA_SYMLINKS += /vendor/firmware_mnt:/firmware
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_VERS := 28.0
-
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
