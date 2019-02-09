6899f21d532c11ab5a87fbfaddc43d0284eff4e9
RuRuTiaSaMa <1009087450@qq.com>
2019-02-09 21:49:19 +0800

    build qcom fmradio
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- msm8998.mk
+++ msm8998.mk
@@ -175,6 +175,12 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.1-service.clearkey 
 
+#FM
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
+
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
         android.hardware.biometrics.fingerprint@2.1-service.nubia_msm8998 
