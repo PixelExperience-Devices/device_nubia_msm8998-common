ceec8ee5b5bb36ce4dfd592f81a660f44932c9af
RuRuTiaSaMa <1009087450@qq.com>
2019-02-19 18:48:39 +0800

    move TARGET_INCLUDE_STOCK_ARCORE to msm8998.mk
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- msm8998.mk
+++ msm8998.mk
@@ -132,6 +132,8 @@ PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # ARcore
+TARGET_INCLUDE_STOCK_ARCORE := true
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/calibration_cad.xml:system/etc/calibration_cad.xml
 
