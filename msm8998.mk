wight554/device_xiaomi_msm8998-common

msm8998-common: import usb hal from hardware/interfaces
Copied from 01787f47f4ee024889c13ec3a2b30624f6d5745d

Change-Id: I85c41fa0844adea68336a368de3bec1f5a60d3d9

msm8998.mk
@@ -327,7 +327,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.xiaomi_msm8998
 
 # Vibrator
 PRODUCT_PACKAGES += \

PixelExperience-Devices/device_nubia_msm8998-common

Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

msm8998.mk
@@ -407,7 +407,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.nubia_msm8998
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk