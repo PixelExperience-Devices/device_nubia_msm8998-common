wight554/device_xiaomi_msm8998-common

msm8998-common: usb: update to usb@1.1
Change-Id: Iff7aaeaca6f54f53442e89952485afbed2c297c1

msm8998.mk
@@ -327,7 +327,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.xiaomi_msm8998
+    android.hardware.usb@1.1-service.xiaomi_msm8998
 
 # Vibrator
 PRODUCT_PACKAGES += \

usb/Usb.h
@@ -1,8 +1,9 @@
-#ifndef ANDROID_HARDWARE_USB_V1_0_USB_H
-#define ANDROID_HARDWARE_USB_V1_0_USB_H
+#ifndef ANDROID_HARDWARE_USB_V1_1_USB_H
+#define ANDROID_HARDWARE_USB_V1_1_USB_H
 
-#include <android/hardware/usb/1.0/IUsb.h>
-#include <hidl/MQDescriptor.h>
+#include <android/hardware/usb/1.1/IUsb.h>
+#include <android/hardware/usb/1.1/types.h>
+#include <android/hardware/usb/1.1/IUsbCallback.h>
 #include <hidl/Status.h>
 #include <log/log.h>
 
@@ -10,18 +11,25 @@
 #undef LOG_TAG
 #endif
 
-#define LOG_TAG "android.hardware.usb@1.0-service"
+#define LOG_TAG "android.hardware.usb@1.1-service.xiaomi_msm8998"
 #define UEVENT_MSG_LEN 2048
 
 namespace android {
 namespace hardware {
 namespace usb {
-namespace V1_0 {
+namespace V1_1 {
 namespace implementation {
 
-using ::android::hardware::usb::V1_0::IUsb;
-using ::android::hardware::usb::V1_0::IUsbCallback;
 using ::android::hardware::usb::V1_0::PortRole;
+using ::android::hardware::usb::V1_0::PortRoleType;
+using ::android::hardware::usb::V1_0::PortDataRole;
+using ::android::hardware::usb::V1_0::PortPowerRole;
+using ::android::hardware::usb::V1_0::Status;
+using ::android::hardware::usb::V1_1::IUsb;
+using ::android::hardware::usb::V1_1::IUsbCallback;
+using ::android::hardware::usb::V1_1::PortMode_1_1;
+using ::android::hardware::usb::V1_1::PortStatus_1_1;
+using ::android::hidl::base::V1_0::DebugInfo;
 using ::android::hidl::base::V1_0::IBase;
 using ::android::hardware::hidl_array;
 using ::android::hardware::hidl_memory;
@@ -34,19 +42,21 @@ using ::android::sp;
 struct Usb : public IUsb {
     Usb();
     Return<void> switchRole(const hidl_string& portName, const PortRole& role) override;
-    Return<void> setCallback(const sp<IUsbCallback>& callback) override;
+    Return<void> setCallback(const sp<V1_0::IUsbCallback>& callback) override;
     Return<void> queryPortStatus() override;
 
-    sp<IUsbCallback> mCallback;
-    private:
-        pthread_t mPoll;
-        pthread_mutex_t mLock = PTHREAD_MUTEX_INITIALIZER;
+    sp<V1_0::IUsbCallback> mCallback_1_0;
+    pthread_t mPoll;
+    // Protects mCallback variable
+    pthread_mutex_t mLock = PTHREAD_MUTEX_INITIALIZER;
+    // Protects roleSwitch operation
+    pthread_mutex_t mRoleSwitchLock = PTHREAD_MUTEX_INITIALIZER;
 };
 
 }  // namespace implementation
-}  // namespace V1_0
+}  // namespace V1_1
 }  // namespace usb
 }  // namespace hardware
 }  // namespace android
 
-#endif  // ANDROID_HARDWARE_USB_V1_0_USB_H
+#endif  // ANDROID_HARDWARE_USB_V1_1_USB_H

PixelExperience-Devices/device_nubia_msm8998-common

Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

msm8998.mk
@@ -407,7 +407,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.nubia_msm8998
+    android.hardware.usb@1.1-service.nubia_msm8998
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk

usb/Usb.h
@@ -1,8 +1,9 @@
-#ifndef ANDROID_HARDWARE_USB_V1_0_USB_H
-#define ANDROID_HARDWARE_USB_V1_0_USB_H
+#ifndef ANDROID_HARDWARE_USB_V1_1_USB_H
+#define ANDROID_HARDWARE_USB_V1_1_USB_H
 
-#include <android/hardware/usb/1.0/IUsb.h>
-#include <hidl/MQDescriptor.h>
+#include <android/hardware/usb/1.1/IUsb.h>
+#include <android/hardware/usb/1.1/types.h>
+#include <android/hardware/usb/1.1/IUsbCallback.h>
 #include <hidl/Status.h>
 #include <log/log.h>
 
@@ -10,18 +11,25 @@
 #undef LOG_TAG
 #endif
 
-#define LOG_TAG "android.hardware.usb@1.0-service"
+#define LOG_TAG "android.hardware.usb@1.1-service.nubia_msm8998"
 #define UEVENT_MSG_LEN 2048
 
 namespace android {
 namespace hardware {
 namespace usb {
-namespace V1_0 {
+namespace V1_1 {
 namespace implementation {
 
-using ::android::hardware::usb::V1_0::IUsb;
-using ::android::hardware::usb::V1_0::IUsbCallback;
 using ::android::hardware::usb::V1_0::PortRole;
+using ::android::hardware::usb::V1_0::PortRoleType;
+using ::android::hardware::usb::V1_0::PortDataRole;
+using ::android::hardware::usb::V1_0::PortPowerRole;
+using ::android::hardware::usb::V1_0::Status;
+using ::android::hardware::usb::V1_1::IUsb;
+using ::android::hardware::usb::V1_1::IUsbCallback;
+using ::android::hardware::usb::V1_1::PortMode_1_1;
+using ::android::hardware::usb::V1_1::PortStatus_1_1;
+using ::android::hidl::base::V1_0::DebugInfo;
 using ::android::hidl::base::V1_0::IBase;
 using ::android::hardware::hidl_array;
 using ::android::hardware::hidl_memory;
@@ -34,19 +42,21 @@ using ::android::sp;
 struct Usb : public IUsb {
     Usb();
     Return<void> switchRole(const hidl_string& portName, const PortRole& role) override;
-    Return<void> setCallback(const sp<IUsbCallback>& callback) override;
+    Return<void> setCallback(const sp<V1_0::IUsbCallback>& callback) override;
     Return<void> queryPortStatus() override;
 
-    sp<IUsbCallback> mCallback;
-    private:
-        pthread_t mPoll;
-        pthread_mutex_t mLock = PTHREAD_MUTEX_INITIALIZER;
+    sp<V1_0::IUsbCallback> mCallback_1_0;
+    pthread_t mPoll;
+    // Protects mCallback variable
+    pthread_mutex_t mLock = PTHREAD_MUTEX_INITIALIZER;
+    // Protects roleSwitch operation
+    pthread_mutex_t mRoleSwitchLock = PTHREAD_MUTEX_INITIALIZER;
 };
 
 }  // namespace implementation
-}  // namespace V1_0
+}  // namespace V1_1
 }  // namespace usb
 }  // namespace hardware
 }  // namespace android
 
-#endif  // ANDROID_HARDWARE_USB_V1_0_USB_H
+#endif  // ANDROID_HARDWARE_USB_V1_1_USB_H