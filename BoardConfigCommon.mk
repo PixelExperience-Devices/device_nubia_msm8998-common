37209c96d19c039d9f1d5484a0e07cb518f41ba3
RuRuTiaSaMa <1009087450@qq.com>
2019-02-24 19:58:48 +0800

    msm8998-common:Enable selinux now
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,7 +59,7 @@ BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 ea
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048 androidboot.usbconfigfs=true 
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3 
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
