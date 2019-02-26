7b75b4bed5dbb6b062a3344680807434521d06d9
ShevT <evgeny.shishkov@gmail.com>
2019-02-26 07:29:41 +0300

    msm8998-common: Use custom healthd charger_res_images
    * Graphics are taken from stock Sony LP
    
    Change-Id: Ifd2725f5dfede41f6cac021daaa3e243237aa701
    
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- msm8998.mk
+++ msm8998.mk
@@ -219,8 +219,7 @@ PRODUCT_COPY_FILES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service \
-    chargeonlymode
+    android.hardware.health@2.0-service 
 
 DEVICE_FRAMEWORK_MANIFEST_FILE += \
     system/libhidl/vintfdata/manifest_healthd_exclude.xml
