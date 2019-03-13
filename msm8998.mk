5296b0ffe28a17631f4f197475bfa0f651957b38
RuRuTiaSaMa <1009087450@qq.com>
2019-03-13 21:33:19 +0800

    delete useless powerhint.xml
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- msm8998.mk
+++ msm8998.mk
@@ -314,7 +314,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service.nubia_msm8998-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml\
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Low power Whitelist
