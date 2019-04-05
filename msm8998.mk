wight554/device_xiaomi_msm8998-common

msm8998-common: Update power HAL to version 1.3.
This adds support for the EXPENSIVE_RENDERING power hint.

BUG: 110112323
Test: adb shell cat /sys/class/kgsl/kgsl-3d0/devfreq/cur_freq
      Verified GPU frequency reset by adb shell pkill surfaceflinger and system_server
Change-Id: Ia1f6e99b67c5e88b610412ad30f152d241095a48
Merged-In: Ia1f6e99b67c5e88b610412ad30f152d241095a48

msm8998.mk
@@ -280,7 +280,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.xiaomi_msm8998-libperfmgr
+    android.hardware.power@1.3-service.xiaomi_msm8998-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json

power-libperfmgr/InteractionHandler.cpp
@@ -16,7 +16,7 @@
 
 //#define LOG_NDEBUG 0
 
-#define LOG_TAG "android.hardware.power@1.2-service.xiaomi_msm8998-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.xiaomi_msm8998-libperfmgr"
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 
 #include <fcntl.h>

power-libperfmgr/Power.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.2-service.xiaomi_msm8998-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.xiaomi_msm8998-libperfmgr"
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
@@ -34,7 +34,7 @@ extern struct stat_pair rpm_stat_map[];
 namespace android {
 namespace hardware {
 namespace power {
-namespace V1_2 {
+namespace V1_3 {
 namespace implementation {
 
 using ::android::hardware::power::V1_0::Feature;
@@ -269,8 +269,30 @@ Return<void> Power::powerHintAsync_1_2(PowerHint_1_2 hint, int32_t data) {
     return Void();
 }
 
+// Methods from ::android::hardware::power::V1_3::IPower follow.
+Return<void> Power::powerHintAsync_1_3(PowerHint_1_3 hint, int32_t data) {
+    if (!isSupportedGovernor() || !mReady) {
+        return Void();
+    }
+
+    if (hint == PowerHint_1_3::EXPENSIVE_RENDERING) {
+        if (mSustainedPerfModeOn) {
+            return Void();
+        }
+
+        if (data > 0) {
+            mHintManager->DoHint("EXPENSIVE_RENDERING");
+        } else {
+            mHintManager->EndHint("EXPENSIVE_RENDERING");
+        }
+    } else {
+        return powerHintAsync_1_2(static_cast<PowerHint_1_2>(hint), data);
+    }
+
+    return Void();
+}
 }  // namespace implementation
-}  // namespace V1_2
+}  // namespace V1_3
 }  // namespace power
 }  // namespace hardware
 }  // namespace android

power-libperfmgr/power-helper.c
@@ -28,7 +28,7 @@
  */
 
 #define LOG_NIDEBUG 0
-#define LOG_TAG "android.hardware.power@1.2-service.xiaomi_msm8998-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.xiaomi_msm8998-libperfmgr"
 
 #include <errno.h>
 #include <inttypes.h>

power-libperfmgr/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.2-service.xiaomi_msm8998-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.xiaomi_msm8998-libperfmgr"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
@@ -28,8 +28,8 @@ using android::OK;
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
-using android::hardware::power::V1_2::IPower;
-using android::hardware::power::V1_2::implementation::Power;
+using android::hardware::power::V1_3::IPower;
+using android::hardware::power::V1_3::implementation::Power;
 
 int main(int /* argc */, char** /* argv */) {
     ALOGI("Power HAL is starting");

PixelExperience-Devices/device_nubia_msm8998-common

Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

msm8998.mk
@@ -316,7 +316,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.nubia_msm8998-libperfmgr
+    android.hardware.power@1.3-service.nubia_msm8998-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json

power-libperfmgr/InteractionHandler.cpp
@@ -16,7 +16,7 @@
 
 //#define LOG_NDEBUG 0
 
-#define LOG_TAG "android.hardware.power@1.2-service.nubia_msm8998-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.nubia_msm8998-libperfmgr"
 #define ATRACE_TAG (ATRACE_TAG_POWER | ATRACE_TAG_HAL)
 
 #include <fcntl.h>

power-libperfmgr/Power.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.2-service.nubia_msm8998-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.nubia_msm8998-libperfmgr"
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
@@ -34,7 +34,7 @@ extern struct stat_pair rpm_stat_map[];
 namespace android {
 namespace hardware {
 namespace power {
-namespace V1_2 {
+namespace V1_3 {
 namespace implementation {
 
 using ::android::hardware::power::V1_0::Feature;
@@ -269,8 +269,30 @@ Return<void> Power::powerHintAsync_1_2(PowerHint_1_2 hint, int32_t data) {
     return Void();
 }
 
+// Methods from ::android::hardware::power::V1_3::IPower follow.
+Return<void> Power::powerHintAsync_1_3(PowerHint_1_3 hint, int32_t data) {
+    if (!isSupportedGovernor() || !mReady) {
+        return Void();
+    }
+
+    if (hint == PowerHint_1_3::EXPENSIVE_RENDERING) {
+        if (mSustainedPerfModeOn) {
+            return Void();
+        }
+
+        if (data > 0) {
+            mHintManager->DoHint("EXPENSIVE_RENDERING");
+        } else {
+            mHintManager->EndHint("EXPENSIVE_RENDERING");
+        }
+    } else {
+        return powerHintAsync_1_2(static_cast<PowerHint_1_2>(hint), data);
+    }
+
+    return Void();
+}
 }  // namespace implementation
-}  // namespace V1_2
+}  // namespace V1_3
 }  // namespace power
 }  // namespace hardware
 }  // namespace android

power-libperfmgr/power-helper.c
@@ -28,7 +28,7 @@
  */
 
 #define LOG_NIDEBUG 0
-#define LOG_TAG "android.hardware.power@1.2-service.nubia_msm8998-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.nubia_msm8998-libperfmgr"
 
 #include <errno.h>
 #include <inttypes.h>

power-libperfmgr/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.2-service.nubia_msm8998-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.nubia_msm8998-libperfmgr"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
@@ -28,8 +28,8 @@ using android::OK;
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
-using android::hardware::power::V1_2::IPower;
-using android::hardware::power::V1_2::implementation::Power;
+using android::hardware::power::V1_3::IPower;
+using android::hardware::power::V1_3::implementation::Power;
 
 int main(int /* argc */, char** /* argv */) {
     ALOGI("Power HAL is starting");