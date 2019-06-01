0fa494e789eb8bb3f36eda31d11e7f5fe6d49ea1
RuRuTiaSaMa <1009087450@qq.com>
2019-06-01 10:59:48 +0800

    move all *.rc to vendor
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,8 +5,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.nubia.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := root/init.nubia.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES    :=vendor/etc/init/hw/init.nubia.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
